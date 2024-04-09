d744e3b171b1f5d4bffa942cc4a0d2b2968bce98
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-04-09 18:25:00 +0900

    fix : BoardDetail 오류 해결

--- src/components/unit/board/detail/BoardDetail.container.js
+++ src/components/unit/board/detail/BoardDetail.container.js
@@ -1,5 +1,7 @@
 import { useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
+import { FETCH_BOARD } from "./BoardDetail.queries";
+import BoardDetailUI from "./BoardDetail.presenter";
 
 export default function BoardDetail() {
   const router = useRouter();
